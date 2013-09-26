770a7fa2651fc745d0dc04f6438b9a8e7369aa21
commander-clifford <droffilcnoslen@gmail.com>
2013-09-26 07:48:58 -0700

    browse page
    Built Browseable sections

--- screens/browse.php
+++ screens/browse.php
@@ -1,12 +1,97 @@
 <div class="main wrapper clearfix">
-    <article class="clearfix ">
-        <section class="cols2-3rd testBox">
+    <article class="cols2-3rd clearfix ">
+        <section class="qpost clearfix">
+        <div class="qpostinfo">
+        <div class="iframe"><center><i class="icon-user"></i></center></div><p>UserName</p><p>Post Date</p></div>
+        <form id="" class="clearfix" action="index.php?page=browse">
+            <legend for="upost">How do I quit a job without burning the bridge?</legend>
+            <textarea rows="6" cols="20" name="upost" id=""></textarea>
+            <input type="submit" name="answer" id="" class="submit clickButton" value="Answer" />
+        </form>
+        </section>
+        <section class="qpost clearfix">
+            <div class="qpostinfo">
+            <div class="iframe"><center><i class="icon-user"></i></center></div><p>UserName</p><p>Post Date</p></div>
+            <form id="" class="clearfix" action="index.php?page=browse">
+                <legend>Which one should I choose?</legend>
+                <input type="radio" name="option"><label class="opitons" for="option">Option Awesome</label>
+                <input type="radio" name="option"><label class="opitons" for="option">Option Cool</label>
+                <input type="radio" name="option"><label class="opitons" for="option">Option Alright</label>
+                <input type="radio" name="option"><label class="opitons" for="option">Option OK</label> 
+                <input type="submit" name="answer" id="" class="submit clickButton" value="Answer" />
+            </form>
+        </section>
+        <section class="qpost clearfix">
+        <div class="qpostinfo">
+        <div class="iframe"><center><i class="icon-user"></i></center></div><p>UserName</p><p>Post Date</p></div>
+        <form id="" class="clearfix" action="index.php?page=browse">
+            <legend for="upost">How do I quit a job without burning the bridge?</legend>
+            <textarea rows="6" cols="20" name="upost" id=""></textarea>
+            <input type="submit" name="answer" id="" class="submit clickButton" value="Answer" />
+        </form>
+        </section>
+        <section class="qpost clearfix">
+            <div class="qpostinfo">
+            <div class="iframe"><center><i class="icon-user"></i></center></div><p>UserName</p><p>Post Date</p></div>
+            <form id="" class="clearfix" action="index.php?page=browse">
+                <legend>Which one should I choose?</legend>
+                <input type="radio" name="option"><label class="opitons" for="option">Option Awesome</label>
+                <input type="radio" name="option"><label class="opitons" for="option">Option Cool</label>
+                <input type="radio" name="option"><label class="opitons" for="option">Option Alright</label>
+                <input type="radio" name="option"><label class="opitons" for="option">Option OK</label> 
+                <input type="submit" name="answer" id="" class="submit clickButton" value="Answer" />
+            </form>
+        </section>
+        <section class="qpost clearfix">
+        <div class="qpostinfo">
+        <div class="iframe"><center><i class="icon-user"></i></center></div><p>UserName</p><p>Post Date</p></div>
+        <form id="" class="clearfix" action="index.php?page=browse">
+            <legend for="upost">How do I quit a job without burning the bridge?</legend>
+            <textarea rows="6" cols="20" name="upost" id=""></textarea>
+            <input type="submit" name="answer" id="" class="submit clickButton" value="Answer" />
+        </form>
+        </section>
+        <section class="qpost clearfix">
+            <div class="qpostinfo">
+            <div class="iframe"><center><i class="icon-user"></i></center></div><p>UserName</p><p>Post Date</p></div>
+            <form id="" class="clearfix" action="index.php?page=browse">
+                <legend>Which one should I choose?</legend>
+                <input type="radio" name="option"><label class="opitons" for="option">Option Awesome</label>
+                <input type="radio" name="option"><label class="opitons" for="option">Option Cool</label>
+                <input type="radio" name="option"><label class="opitons" for="option">Option Alright</label>
+                <input type="radio" name="option"><label class="opitons" for="option">Option OK</label> 
+                <input type="submit" name="answer" id="" class="submit clickButton" value="Answer" />
+            </form>
+        </section>
+        <section class="qpost clearfix">
+        <div class="qpostinfo">
+        <div class="iframe"><center><i class="icon-user"></i></center></div><p>UserName</p><p>Post Date</p></div>
+        <form id="" class="clearfix" action="index.php?page=browse">
+            <legend for="upost">How do I quit a job without burning the bridge?</legend>
+            <textarea rows="6" cols="20" name="upost" id=""></textarea>
+            <input type="submit" name="answer" id="" class="submit clickButton" value="Answer" />
+        </form>
+        </section>
+        <section class="qpost clearfix">
+            <div class="qpostinfo">
+            <div class="iframe"><center><i class="icon-user"></i></center></div><p>UserName</p><p>Post Date</p></div>
+            <form id="" class="clearfix" action="index.php?page=browse">
+                <legend>Which one should I choose?</legend>
+                <input type="radio" name="option"><label class="opitons" for="option">Option Awesome</label>
+                <input type="radio" name="option"><label class="opitons" for="option">Option Cool</label>
+                <input type="radio" name="option"><label class="opitons" for="option">Option Alright</label>
+                <input type="radio" name="option"><label class="opitons" for="option">Option OK</label> 
+                <input type="submit" name="answer" id="" class="submit clickButton" value="Answer" />
+            </form>
+        </section>
+
+        
+    </article>
+    <article class="cols1-3rd clearfix">
+        <section class="qpost">
             <h2>article section h2</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor.</p>
         </section>
-        <section class="cols1-3rd testBox">
-            <h2>article section h2</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor.</p>
-        </section>
+
     </article>
 </div>
